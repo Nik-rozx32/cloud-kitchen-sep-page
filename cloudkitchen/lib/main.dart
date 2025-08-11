f37abdcdd85818a216e4ebd3491c4d9005fa3f25
Nik-rozx32 <xma.nikkitha2020@gmail.com>
2025-08-11 21:37:17 +0530

    new fixes

--- cloudkitchen/lib/main.dart
+++ cloudkitchen/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloudkitchen/order_page.dart/order_accepting_page.dart';
 import 'package:flutter/material.dart';
 import 'order_page.dart/order_accepting_page.dart';
+
 void main(){
   runApp(MyApp());
 }
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OrderAcceptPage(),
+      home: MainNavigationScreen(),
     );
   }
 }
